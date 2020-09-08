3fcb7ce993e251767c94eb99eb6f3ae08482dd06
DESKTOP-4M9HJ2O\ADMIN <minhhieunn@gmail.com>
2020-09-08 23:56:46 +0700

    add payment system singleton variable

--- src/main/scala/PaymentSystemNode.scala
+++ src/main/scala/PaymentSystemNode.scala
@@ -1,3 +1,13 @@
-class PaymentSystemNode(port: Int,shouldStartSingleton: Boolean = true) extends App {
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.cluster.typed.{ClusterSingleton, SingletonActor}
 
+class PaymentSystemNode(port: Int,shouldStartSingleton: Boolean = true) extends App {
+  def apply(): Behavior[Nothing] = Behaviors.setup[Nothing]{context =>
+
+      val paymentSystem: ActorRef[Transaction] = ClusterSingleton(context.system)
+        .init(SingletonActor(PaymentSystem(),"paymentSystem"))
+    
+    Behaviors.empty
+  }
 }
